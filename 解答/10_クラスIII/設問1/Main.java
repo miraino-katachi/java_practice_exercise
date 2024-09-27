a2f6c194a0aeb18ef6439fc6bf3b8cfb0faaa520
坂上　正 <t_sakagami@miraino-katachih2.co.jp>
2024-09-27 09:06:16 +0900

    修正

--- 解答/10_クラスIII/設問1/Main.java
+++ 解答/10_クラスIII/設問1/Main.java
@@ -1,19 +1,19 @@
 public class Main {
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        //  戦闘機クラスのインスタンスの生成
-        FighterAirCraft fighter = new FighterAirCraft();
-        //  旅客機クラスのインスタンスの生成
-        PassengerPlane airliner = new PassengerPlane();
+		//  戦闘機クラスのインスタンスの生成
+		FighterAirCraft fighter = new FighterAirCraft();
+		//  旅客機クラスのインスタンスの生成
+		PassengerPlane airliner = new PassengerPlane();
 
-        //  戦闘機クラスの処理
-        System.out.println("戦闘機");
-        fighter.fly();                  //  飛行する
-        fighter.fight();                //  戦闘する
+		//  戦闘機クラスの処理
+		System.out.println(fighter.getType());
+		fighter.fly();                  //  飛行する
+		fighter.fight();                //  戦闘する
 
-        //  旅客機クラスの処理
-        System.out.println("旅客機");
-        airliner.fly();                //  飛行する
-        airliner.carryPassengers();    //  乗客を運ぶ
-    }
+		//  旅客機クラスの処理
+		System.out.println(airliner.getType());
+		airliner.fly();                //  飛行する
+		airliner.carryPassengers();    //  乗客を運ぶ
+	}
 }
